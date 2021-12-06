2d799abdf20553d9d577f457be36aded1087d483
D2TA <66340798+D2TA@users.noreply.github.com>
2021-12-06 18:41:21 +0000

    improved dark mode and light mode.

--- script-html.js
+++ script-html.js
@@ -14,9 +14,57 @@ function expandhistory() {
 function darkmode() {
   document.body.style.color = 'white'
   document.body.style.backgroundColor = '#121212'
+  // document.body.style.backgroundColor = '#121212'
+  var c = document.body.children;
+  for (let index = 0+2; index < c.length; index++ ){
+    c[index].style.borderBottom = "thin dotted white";
+  };
 }
 
 function lightmode() {
   document.body.style.color = '#121212'
   document.body.style.backgroundColor = 'white'
+  var c = document.body.children;
+  for (let index = 0+2; index < c.length; index++ ){
+    c[index].style.borderBottom = "thin dotted #121212" ;
+  };
 }
+
+function UpdateIndicator(id, value) {
+  if (value == -1) {
+    // Down
+    document.getElementById(id + 'Down').style.visibility = 'visible'
+    document.getElementById(id + 'Down').style.display = 'initial';
+    // Up
+    document.getElementById(id + 'Up').style.visibility = 'hidden'
+    document.getElementById(id + 'Up').style.display = 'none'
+    document.getElementById(id + 'Flat').style.visibility = 'hidden'
+    document.getElementById(id + 'Flat').style.display = 'none'
+    
+  } else if (value == 1) {
+    // Up
+    document.getElementById(id + 'Up').style.visibility = 'visible'
+    document.getElementById(id + 'Up').style.display = 'initial'
+    // Down
+    document.getElementById(id + 'Down').style.visibility = 'hidden'
+    document.getElementById(id + 'Down').style.display = 'none'
+    document.getElementById(id + 'Flat').style.visibility = 'hidden'
+    document.getElementById(id + 'Flat').style.display = 'none'
+
+  } else {
+    // Up
+    document.getElementById(id + 'Flat').style.visibility = 'visible'
+    document.getElementById(id + 'Flat').style.display = 'initial'
+    // Down
+    document.getElementById(id + 'Down').style.visibility = 'hidden'
+    document.getElementById(id + 'Down').style.display = 'none'
+    document.getElementById(id + 'Up').style.visibility = 'hidden'
+    document.getElementById(id + 'Up').style.display = 'none'
+  };
+};
+
+
+
+
+id = 'marketIndicator'
+value = 1
